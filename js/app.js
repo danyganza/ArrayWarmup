55eeb451a04acfc26c2d1ef13e29c95ddf7c8b5d
dannyganza <kingdanny@Dannys-Air.hitronhub.home>
2023-03-18 10:18:04 -0600

    added errays

---
+++ js/app.js
@@ -0,0 +1,31 @@
+let student_name = "Ganza";
+let student_gpa = 3.2;
+let is_clever = true;
+
+if (student_gpa >= 3.0) {
+    console.log(`you're a smart man`)
+} else if (student_gpa <= 2.5) {
+    console.log(`work harder bud`)
+}
+
+if (student_gpa >= 0 - 1.9) {
+    console.log(`F`)
+}
+
+if (student_gpa >= 2.0 - 2.4) {
+    console.log(`D`)
+}
+
+if (student_gpa >= 2.5 - 2.9) {
+    console.log(`C`)
+}
+
+if (student_gpa >= 3.0 - 3.4) {
+    console.log(`B`)
+}
+
+if (student_gpa >= 3.5 - 4.0) {
+    console.log(`A`)
+}
+
+//creating arrays 
